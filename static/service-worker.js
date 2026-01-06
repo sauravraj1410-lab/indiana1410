90cd8ad15cbd98fd789a6ca0c9ceab9623fe57b3
Saurav Kumar <sauravraj1410@gmail.com>
2026-01-06 13:37:19 +0530

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,22 +1,5 @@
-const CACHE_NAME = "pwa-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("fetch", event => {});
