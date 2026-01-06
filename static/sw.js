4bb57606237d44424a8d956fcb7302375d859d01
Saurav Kumar <sauravraj1410@gmail.com>
2026-01-06 19:15:12 +0530

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,2 +1,7 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
